550e04f3d1844609710e38fb1aede54bb6e80792
Javier Alfonso <jalfonsocu@gmail.com>
2022-03-21 16:19:43 -0400

    Casi funcional

--- Main.gd
+++ Main.gd
@@ -1,19 +1,16 @@
 extends Node2D
 
+var tilemap :TileMap
+var tilemap_puntos :TileMap
+var game_enabled_row = 1
+var code_to_evaluate = [0,0,0,0,0]
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var tilemap
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tilemap = get_node( "TileMap")
-	
-	print(Global.code)
-	
-
+	tilemap_puntos = get_node("TileMapPuntos")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -26,9 +23,40 @@ func _input(event):
 			var mouse :Vector2 = get_global_mouse_position()
 			var cell :Vector2 = tilemap.world_to_map(mouse)
 		
-			print(cell)
-		
 			var abc :int = tilemap.get_cellv(cell)
-			var new_abc :int = (abc + 1) % 9 
-			tilemap.set_cellv(cell, new_abc)
+			if abc > -1 and cell[1] == game_enabled_row:
+				var new_abc :int = (abc + 1) % 9 
+				code_to_evaluate[cell[0]] = new_abc
+				tilemap.set_cellv(cell, new_abc)
+
+
+func _on_ButtonProbar_pressed():
+	print(code_to_evaluate)
+	print(Global.code)
+	
+	if code_to_evaluate[0] != 0 and code_to_evaluate[1] != 0 and code_to_evaluate[2] != 0 and code_to_evaluate[3] != 0 and code_to_evaluate[4] != 0:
+		if code_to_evaluate == Global.code:
+			print("iguales") # dar estado ganador
+		else:
+			var blancas = 0
+			var negras = 0
+			var temporal_code = code_to_evaluate
+			
+			for i in range(5):
+				for j in range(5):
+					if Global.code[i] == code_to_evaluate[j] && temporal_code[j] != -1:
+						if(i == j):
+							negras = negras +1
+						else:
+							blancas = blancas + 1 
+						temporal_code[j] = -1
+			
+			#var cx :Vector2 = [[game_enabled_row,5]]
+			tilemap_puntos.set_cellv(Vector2(5, game_enabled_row), blancas )
+			tilemap_puntos.set_cellv(Vector2(6, game_enabled_row), negras + 5 +1)
+			
+			game_enabled_row = game_enabled_row + 1
+			code_to_evaluate = [0,0,0,0,0]
+	else:
+		print("falta completar") # reemplazar con algo
 	
